91661dfe644701b3a8a6b16bc3b8f2a034e16e30
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2021-05-14 20:38:54 +0800

    Refactored code

--- Client/app/Main.js
+++ Client/app/Main.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter, Switch, Route}  from 'react-router-dom'
 
@@ -6,24 +6,30 @@ import {BrowserRouter, Switch, Route}  from 'react-router-dom'
 
 import Header from './components/Header'
 import HomeGuest from './components/HomeGuest'
+import Home from './components/Home'
 import Footer from './components/Footer'
 import About from './components/About'
 import Terms from './components/Terms'
+import CreatePost from './components/CreatePost'
+
 
 
 
 function Main() {
+    // check if localstorage has the token and convert it to true so to render the header logged in
+    const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('token')));
+
   return (
     <BrowserRouter>
-    <Header />
+    <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
     <Switch>
-    <Route path='/' exact><HomeGuest /></Route>
+    <Route path='/' exact> {loggedIn ? <Home /> : <HomeGuest />} </Route>
+    <Route path='/create-post'><CreatePost /></Route>
     <Route path='/about-us' exact><About /></Route>
     <Route path='/terms' exact><Terms /></Route>
     </Switch>
     <Footer />
     </BrowserRouter>
-
   )
 }
 
---
+++ Client/app/components/HeaderLoggedOut.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+const HeaderLoggedOut = (props) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await Axios.post('http://localhost:4000/login', {
+        username,
+        password,
+      });
+      if (response.data) {
+        console.log(response.data);
+        props.setLoggedIn(true);
+        localStorage.setItem('username', response.data.username);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('avatar', response.data.avatar);
+      } else {
+        console.log('incorrect username or password');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <form className="mb-0 pt-2 pt-md-0" onSubmit={handleSubmit}>
+      <div className="row align-items-center">
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input name="username" onChange={(e) => setUsername(e.target.value)} className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+        </div>
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input name="password" onChange={(e) => setPassword(e.target.value)} className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+        </div>
+        <div className="col-md-auto">
+          <button className="btn btn-success btn-sm">Sign In</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default HeaderLoggedOut;
