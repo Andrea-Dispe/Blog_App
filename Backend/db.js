ed6bd31552a0adedfb282bf48c96bd3eba434e1a
Andrea Andrea <dispe.andrea@gmail.com>
2021-09-26 16:47:20 +0200

    Readme Added

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,14 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const mongodb = require("mongodb")
+const dotenv = require('dotenv');
+dotenv.config();
+const mongodb = require('mongodb');
 
-mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  module.exports = client
-  const app = require("./app")
-  app.listen(process.env.PORT)
-  console.log('db connected')
-
-})
+  module.exports = client;
+  const app = require('./app');
+  app.listen(process.env.PORT);
+  console.log('db connected');
+});
+  
---
+++ README.md
@@ -0,0 +1,30 @@
+# Blog App
+
+This is a React app made with Webpack and Bootstrap connected to a socket.io server connected to a MongoDB on Atlas.
+
+It is a blog app where users can register with email, username and password, login and create blog posts, read them and make friends requests with other users.
+
+It is composed by a frontend and a backend.
+
+
+
+#### Tech stack (Front-end):
+
+- React
+- Bootstrap
+- Webpack
+
+
+
+#### Tech stack (Back-end):
+
+- Express
+- Socket.io
+- MongoDB with Atlas
+
+
+
+#### To do:
+
+- Implement Redux (data now is passed down with useContext and useReducer)
+
